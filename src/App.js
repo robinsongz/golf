f65b4258d1ef2b99510de4525a222294154adb25
Robin Sung <robin.sung@ibuypower.com>
2020-02-03 13:15:30 -0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
+import Nav from './Nav';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Button variant="contained" color="primary">
-        Testing
-      </Button>
+      <Nav />
     </div>
   );
 }
